94b484cd67df606f64612acc6b55fc732df4adf3
Jenifer0498 <jeniferramasamy004@gmail.com>
2023-01-24 12:56:17 +0530

    new branch

--- src/test/resources/Sample.java
+++ src/test/resources/Sample.java
@@ -8,6 +8,11 @@ System.out.println("Bless");
 
 private void tc1() {
 	System.out.println("You Always");
-	
+private void tc1() {
+	System.out.println("Dad");
+private void tc1() {
+	System.out.println("is my ");
+private void tc1() {
+	System.out.println("Hero");
 }
 }
