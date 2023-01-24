8441850426df4a7c0fa1a5912382771b27fc8669
Jenifer0498 <jeniferramasamy004@gmail.com>
2023-01-24 12:28:45 +0530

    Frame code is added

---
+++ src/test/resources/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+private void tc1() {
+System.out.println("Jesus");
+
+private void tc1() {
+System.out.println("Bless");
+
+private void tc1() {
+	System.out.println("You Always");
+	
+}
+}
